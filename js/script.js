93266c657497c5e38edaadf3134c06cb653cb4d9
Miyaki <144659450+sendjiMiyaki@users.noreply.github.com>
2023-10-10 09:11:29 +0000

    + read me

--- js/script.js
+++ js/script.js
@@ -57,3 +57,82 @@ function countUp() {
 function reset() {
   counters.forEach((counter) => (counter.innerHTML = '0'));
 }
+
+/******************************FORMULAIRE SIGN UP**************************************/
+
+/*champs à remplir*/
+
+let prenom = document.getElementById("prenom");
+let nom = document.getElementById("nom");
+let email = document.getElementById("email");
+let mdp = document.getElementById("mdp");
+let message = document.getElementById("message");
+
+/*selectionner la case à cocher et le texte à côté*/
+let cocher = document.getElementById('formCheck-1');
+let age = document.querySelector('label[for="formCheck-1"]');
+
+/*afficher le message invisible pour le mdp*/
+let messageErreur = document.querySelector(".text-danger");
+
+function verifierFormulaire() {
+    if (prenom.value === "") {
+      prenom.style.borderColor = "red";
+    } else {
+      prenom.style.borderColor = "green";
+    }
+
+    if (nom.value === "") {
+      nom.style.borderColor = "red";
+    } else {
+      nom.style.borderColor = "green";
+    }
+    
+    /*condition : si email.value contient "@"*/
+    if (email.value === "" || !email.value.includes("@")) {
+      email.style.borderColor = "red";
+    } else if (email.value.includes("@")) {
+      email.style.borderColor = "green";
+    }
+
+    /*condition : mdp > 8 caractères sinon affichage de "text-danger invisible"*/
+    if (mdp.value === "" || mdp.value.length < 8) {
+      mdp.style.borderColor = "red";
+      messageErreur.classList.remove("invisible");
+    } else if (mdp.value.length >= 8) {
+      mdp.style.borderColor = "green";
+      messageErreur.classList.add("invisible");
+    }
+
+    if (message.value === "") {
+      message.style.borderColor = "red";
+    } else {
+      message.style.borderColor = "green";
+    }
+
+    if (!cocher.checked) {
+      age.style.color = "red";
+    } else if (cocher.checked) {
+      age.style.color = "green";
+    }
+}
+
+/******************************FORMULAIRE SIGN IN -> ("email2" & "mdp2")**************************************/
+
+function verifierFormulaire2() {
+    /*condition : si email.value contient "@"*/
+    if (email.value === "" || !email.value.includes("@")) {
+      email.style.borderColor = "red";
+    } else if (email.value.includes("@")) {
+      email.style.borderColor = "green";
+    }
+
+    /*condition : mdp > 8 caractères sinon affichage de "text-danger invisible"*/
+    if (mdp.value === "" || mdp.value.length < 8) {
+      mdp.style.borderColor = "red";
+      messageErreur.classList.remove("invisible");
+    } else if (mdp.value.length >= 8) {
+      mdp.style.borderColor = "green";
+      messageErreur.classList.add("invisible");
+    }
+}
